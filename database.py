fdf6fae018db267d985dc20c471bc298aee2fd5e
ISHIKA GANGWAL <ishikagangwal06@gmail.com>
2026-07-16 22:37:47 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,1 @@
-import psycopg2
 
-def connect_db():
-
-    connection = psycopg2.connect(
-        host="localhost",
-        database="BankManagement",
-        user="postgres",
-        password="1234",
-        port="5433"
-    )
-
-    return connection
